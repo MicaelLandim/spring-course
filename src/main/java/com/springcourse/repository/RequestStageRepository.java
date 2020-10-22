10ee31496a3be80688c5608dc41e08f1cd8d5f4a
Micael Landim <landim.m.s@gmail.com>
2020-10-22 13:37:19 -0300

    Testes com JUnity

---
+++ src/main/java/com/springcourse/repository/RequestStageRepository.java
@@ -0,0 +1,21 @@
+package com.springcourse.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.springcourse.domain.Request;
+import com.springcourse.domain.RequestStage;
+import com.springcourse.domain.enums.RequestState;
+
+@Repository
+public interface RequestStageRepository extends JpaRepository<RequestStage, Long> {
+	
+	public List<Request> findAllByRequestId(Long id);
+	
+	@Query("UPDATE request_stage SET state = ?2 WHERE id = ?1")
+	public Request updateStatus(Long id, RequestState state);
+
+} 
