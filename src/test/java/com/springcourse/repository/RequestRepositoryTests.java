464e23883fa1ecff49aaf6ecb250988ad92b7538
Micael Landim <landim.m.s@gmail.com>
2020-10-23 09:49:41 -0300

    Mudanças

---
+++ src/test/java/com/springcourse/repository/RequestRepositoryTests.java
@@ -0,0 +1,76 @@
+package com.springcourse.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.springcourse.domain.Request;
+import com.springcourse.domain.User;
+import com.springcourse.domain.enums.RequestState;
+import com.springcourse.domain.enums.Role;
+
+@RunWith(SpringRunner.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@SpringBootTest
+public class RequestRepositoryTests {
+	@Autowired
+	private RequestRepository requestRepository;
+
+	@Test
+	public void AsaveTest() {
+		User owner = new User();
+		owner.setId(1L);
+		
+		Request request = new Request(null, "Novo LapTop HP", "Pretendo obter um laptop", new Date(), RequestState.OPEN, owner, null);
+		
+		Request createdRequest = requestRepository.save(request);
+		
+		assertThat(createdRequest.getId()).isEqualTo(1L);
+	}
+
+	@Test
+	public void updateTest() {
+		User owner = new User();
+		owner.setId(1L);
+		
+		Request request = new Request(1L, "Novo LapTop HP", "Pretendo obter um laptop UPDATED", null, RequestState.OPEN, owner, null);
+		
+		Request updatedRequest = requestRepository.save(request);
+		
+		assertThat(updatedRequest.getDescription()).isEqualTo("Pretendo obter um laptop UPDATED");
+	}
+
+	@Test
+	public void getByIdTest() {
+		Optional<Request> result = requestRepository.findById(1L);
+		
+		Request request = result.get();
+		
+		assertThat(request.getSubject()).isEqualTo("Novo LapTop HP");
+	}
+	
+	@Test
+	public void listTest() {
+		List<Request> requests = requestRepository.findAll();
+		
+		assertThat(requests.size()).isEqualTo(1);
+	}
+	
+	@Test
+	public void listByOwnerIdTest() {
+		List<Request> requests = requestRepository.findAllByOwnerId(1L);
+		
+		assertThat(requests.size()).isEqualTo(1);
+	}
+	
+}
